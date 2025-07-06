2c5657da0357ea79618aae1f9c6cf8d28c4b1239
BurakTeli <tellibrk216@gmail.com>
2025-07-07 02:47:11 +0300

    Version 2.0 - Added PeopleService for fetching character data from SWAPI

---
+++ Week12_StarWars/src/services/PeopleService.ts
@@ -0,0 +1,14 @@
+// src/services/PeopleService.ts
+import axios from "axios";
+
+const BASE_URL = "https://www.swapi.tech/api";
+
+export const getPeopleList = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/people`);
+    return response.data.results; // Array of people (name, uid, url)
+  } catch (error) {
+    console.error("Error fetching people:", error);
+    return [];
+  }
+};
