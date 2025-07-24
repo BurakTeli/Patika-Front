21516b0413b3a9a43ebd7209927caa94b7a8d5c1
BurakTeli <tellibrk216@gmail.com>
2025-07-25 00:33:23 +0300

    Version 1.2 - Added placeholders for Books, Borrows, Categories, Publishers pages
    Empty page components created for future development of:
    - Books
    - Borrows
    - Categories
    - Publishers

---
+++ Week15-Graduation-Project/src/pages/BorrowsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BorrowsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BorrowsPage
---
+++ Week15-Graduation-Project/src/pages/PublishersPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PublishersPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PublishersPage
